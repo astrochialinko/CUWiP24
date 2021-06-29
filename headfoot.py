d8a4b4b5104a85efba86727ac442db16edb798bd
TheSkyentist <raphael.hviding@gmail.com>
2021-06-28 20:49:14 -0700

    New Pages

---
+++ headfoot.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+# Import regex
+import re
+
+# Source and Target List
+source = 'index.html'
+targets = ['application.html','codeofconduct.html']
+
+# Tags we will split on
+tags = ['<!-- Header -->','<!-- Footer -->']
+
+# Go through targets and replace
+for target in targets:
+
+    # Open Targets and Files
+    with open(source,'r') as f: s = f.read()
+    f = open(target,'r+')
+    t = f.read()
+    
+    # Iterate over Tags
+    for tag in tags:
+
+        # Replace
+        t = t.split(tag)
+        t[1] = s.split(tag)[1] 
+
+        # Join back p
+        t = tag.join(t)
+
+    f.seek(0) # Go to start of file
+    f.write(t) # Write to file
+    f.truncate() # Get rid of the rest
+    f.close()
